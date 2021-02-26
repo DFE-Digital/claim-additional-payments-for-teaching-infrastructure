c9ddf15692abf3cfc92da5671068ef2049cfc7b3
chandra kasetty <chandra.kasetty@and.digital>
2021-02-26 10:48:40 +0000

    Add read me file

--- claim-additional-payments-for-teaching-qts-api/DQTCsvProcessor.cs
+++ claim-additional-payments-for-teaching-qts-api/DQTCsvProcessor.cs
@@ -1,16 +1,17 @@
 using System;
+using System.IO;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace claim_additional_payments_for_teaching_qts_api
 {
-    public static class DqtCsvProcessor
+    public static class DQTCsvProcessor
     {
-        [FunctionName("DqtCsvProcessor")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        [FunctionName("DQTCsvProcessor")]
+        public static void Run([BlobTrigger("[BLOB-CONTAINER]/{name}", Connection = "")]Stream myBlob, string name, ILogger log)
         {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
         }
     }
 }
