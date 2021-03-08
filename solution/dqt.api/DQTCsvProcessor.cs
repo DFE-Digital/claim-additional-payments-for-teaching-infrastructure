3c7791c0090b2b316ed935023ceaf48be3e386fb
chandra kasetty <chandra.kasetty@and.digital>
2021-03-08 11:10:10 +0000

    Code Tidy-Up

--- solution/dqt.api/DQTCsvProcessor.cs
+++ solution/dqt.api/DQTCsvProcessor.cs
@@ -1,27 +1,15 @@
 using System.IO;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
-using dqt.datalayer.Database;
 
 namespace dqt.api
 {
     public class DQTCsvProcessor
     {
-        private readonly DQTDataContext _context;
-
-        public DQTCsvProcessor(DQTDataContext context)
-        {
-            _context = context;
-        }
-
         [FunctionName("dqt-csv-processor")]
         public void Run([BlobTrigger("dqt-cont/{name}", Connection = "AzureWebJobsStorage")]Stream myBlob, string name, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
-
-            _context.Database.EnsureCreated();
-
-            _context.SaveChanges();
         }
     }
 }
