9b515a5cb8b7add075f8a453469d75faf39f1e03
chandra kasetty <chandra.kasetty@and.digital>
2021-03-22 12:07:19 +0000

    Fix DB Migration

--- solution/dqt.datalayer/Database/DQTDataContext.cs
+++ solution/dqt.datalayer/Database/DQTDataContext.cs
@@ -11,6 +11,7 @@ namespace dqt.datalayer.Database
         internal DbSet<QualifiedTeacher> QualifiedTeachers { get; set; }
         internal DbSet<QualifiedTeacherBackup> QualifiedTeachersBackup { get; set; }
         internal DbSet<DQTFileTransfer> DQTFileTransfer { get; set; }
+
     public DQTDataContext() { }
         public DQTDataContext(DbContextOptions options)
             : base(options)
