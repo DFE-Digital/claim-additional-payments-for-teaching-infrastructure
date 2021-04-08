721d5d14756cf9d356958d3ced799ed7d8703998
lucas.kelly <lucas.kelly@and.digital>
2021-04-08 15:09:52 +0100

    removed verify from containers

--- azure/infra/terraform/modules/container_reg/data.tf
+++ azure/infra/terraform/modules/container_reg/data.tf
@@ -76,7 +76,7 @@ data "azurerm_key_vault_secret" "RollbarAccessToken" {
 #   key_vault_id = data.azurerm_key_vault.secrets_kv.id
 # }
 
-data "azurerm_app_service" "app_vsp_as" {
-  name                = format("%s-%s", var.app_rg_name, "vsp-as")
-  resource_group_name = var.app_rg_name
-}
+# data "azurerm_app_service" "app_vsp_as" {
+#   name                = format("%s-%s", var.app_rg_name, "vsp-as")
+#   resource_group_name = var.app_rg_name
+# }
