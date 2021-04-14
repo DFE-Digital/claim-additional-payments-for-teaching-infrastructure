8ed8c7f2e8b8e4e87ffc91638da7d681bbae635f
lucas.kelly <lucas.kelly@and.digital>
2021-04-14 15:28:53 +0100

    testing removing ip addresses in dev

--- azure/infra/terraform/modules/network/network.tf
+++ azure/infra/terraform/modules/network/network.tf
@@ -3,8 +3,8 @@ resource "azurerm_virtual_network" "projcore_vn_01" {
   resource_group_name   = var.projcore_rg_name
   location              = var.rg_location
   vm_protection_enabled = false
-  address_space         = local.ip_address_space
-  # dns_servers           = local.ip_dns_servers
+  address_space         = data.azurerm_key_vault_secret.vnet_range
+  # dns_servers           = data.azurerm_key_vault_secret.vnet_dns_range
 
   tags = merge({
     },
--- azure/infra/terraform/modules/network/variable.tf
+++ azure/infra/terraform/modules/network/variable.tf
@@ -2,15 +2,7 @@
 # REQUIRED PARAMETERS
 # You must provide a value for each of these parameters.
 # ---------------------------------------------------------------------------------------------------------------------
-# variable "mandatory_variable" {
-#   type        = string
-#   description = "This is mandatory as there is no default declaration"
-# }
 
-# variable "core_rg_name" {
-#   type        = string
-#   description = "Core resource group for the network profile"
-# }
 variable "projcore_rg_name" {
   type        = string
   description = "Project Core resource group for the network profile"
@@ -34,10 +26,3 @@ variable "common_tags" {
 # locals {
 #   calculated_local_value = uuid()
 # }
-
-locals {
-
-  ip_address_space = var.rg_prefix == "s118d01" ? ["11.0.1.0/24"] : var.rg_prefix == "s118t01" ? ["11.0.2.0/24"] : var.rg_prefix == "s118p01" ? ["11.0.3.0/24"] : ["192.168.1.0/24"]
-  ip_dns_servers   = var.rg_prefix == "s118d01" ? ["11.0.1.4", "11.0.1.5"] : var.rg_prefix == "s118t01" ? ["11.0.2.4", "11.0.2.5"] : var.rg_prefix == "s118p01" ? ["11.0.3.4", "11.0.3.5"] : ["192.168.1.4", "192.168.1.5"]
-
-}
