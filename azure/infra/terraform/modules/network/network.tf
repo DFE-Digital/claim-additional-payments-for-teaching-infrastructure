08cd0c60b81760fb17ee768fc2f3160540dd5150
lucas.kelly <lucas.kelly@and.digital>
2021-04-14 15:50:59 +0100

    added square brackets to network address space

--- azure/infra/terraform/modules/network/network.tf
+++ azure/infra/terraform/modules/network/network.tf
@@ -3,7 +3,7 @@ resource "azurerm_virtual_network" "projcore_vn_01" {
   resource_group_name   = var.projcore_rg_name
   location              = var.rg_location
   vm_protection_enabled = false
-  address_space         = data.azurerm_key_vault_secret.vnet_range
+  address_space         = [data.azurerm_key_vault_secret.vnet_range]
   # dns_servers           = data.azurerm_key_vault_secret.vnet_dns_range
 
   tags = merge({
