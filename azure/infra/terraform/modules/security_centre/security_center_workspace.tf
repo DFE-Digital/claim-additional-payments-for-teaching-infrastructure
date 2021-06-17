6d698c80de89683762e23f6a951faddbc5e3ab8f
lucas.kelly <lucas.kelly@and.digital>
2021-06-17 15:54:58 +0100

    removed none used code

--- azure/infra/terraform/modules/security_centre/security_center_workspace.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_security_center_workspace" "sec_centre" {
-  scope        = lower(format("%s%s", "/subscriptions/", data.azurerm_client_config.current.subscription_id))
-  workspace_id = var.la_id
-}
