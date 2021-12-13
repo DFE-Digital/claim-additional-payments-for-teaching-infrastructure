5253847293000afd6ae63e6019621942f8273e3f
lucas.kelly <lucas.kelly@and.digital>
2021-12-13 09:33:58 +0000

    removing dqt temp solution resources

--- azure/infra/terraform/modules/app_services/output.tf
+++ azure/infra/terraform/modules/app_services/output.tf
@@ -3,7 +3,7 @@ output "app_service_plan_id" {
   description = "App Service plan ID"
 }
 
-output "func_app_service_plan_id" {
-  value       = azurerm_app_service_plan.fapp_prem_id.id
-  description = "Function App Service plan ID"
-}
+# output "func_app_service_plan_id" {
+#   value       = azurerm_app_service_plan.fapp_prem_id.id
+#   description = "Function App Service plan ID"
+# }
