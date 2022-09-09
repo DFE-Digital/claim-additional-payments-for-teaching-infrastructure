bfb55d4fe156d831ef7011ac844c65e3ab263e35
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2022-09-09 11:23:15 +0100

    Remove redundant databases
    - DQT is not used anymore
    - All environments have migrated to postgres 11

--- azure/infra/terraform/modules/postgres/firewall_rule.tf
+++ azure/infra/terraform/modules/postgres/firewall_rule.tf
@@ -1,13 +1,3 @@
-resource "azurerm_postgresql_firewall_rule" "filewall_exp" {
-  name                = "AzureServices"
-  resource_group_name = var.app_rg_name
-  server_name         = azurerm_postgresql_server.app_postgres.name
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "0.0.0.0"
-}
-
-################
-
 resource "azurerm_postgresql_firewall_rule" "filewall_exp_11" {
   name                = "AzureServices"
   resource_group_name = var.app_rg_name
