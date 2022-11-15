a2ce5dc0f0e75d1b8a1ff56701615cd17d21a86d
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2022-11-15 16:59:27 +0000

    Remove obsolete terraform modules

--- azure/infra/terraform/modules/nat_gateway/gateway_assoc.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_subnet_nat_gateway_association" "sub_nat_assoc_01" {
-  subnet_id      = data.azurerm_subnet.subnet_01.id
-  nat_gateway_id = azurerm_nat_gateway.nat_gateway_01.id
-}
